e8333fa6f3ee0c46b8d47e42a9c0364a5958e391
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-02-18 15:31:22 -0700

    Changes to make functional with child process restarts

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 var io = require('socket.io');
 var DEFAULT_PORT = 8999;
 var fs = require('fs');
+var q = require('q');
 
 var nodePath = require('path');
 
-console.log("PLUGIN BEING RELOADED!");
-
 exports = module.exports = function(hotReload, config) {
+    var port = config.port || DEFAULT_PORT;
 
-    console.log("REFRESH PLUGIN BEGIN");
-    exports.port = config.port || DEFAULT_PORT;
+    var io = require('socket.io').listen(port, { log: false });
 
-    exports.html = fs
-        .readFileSync(nodePath.join(__dirname, 'refresh.html'), {encoding: 'utf8'})
-        .replace(/PORT/g, exports.port);
-
-    var io = require('socket.io').listen(exports.port, { log: false }, function() {
-        console.log("REFRESH PLUGIN ENABLED");
-        exports.enabled = true;
-    });
+    var readyEvent = config['ready-event'] || 'ready';
 
     io.sockets.on('connection', function (socket) {
 
@@ -34,8 +26,21 @@ exports = module.exports = function(hotReload, config) {
             hotReload.removeListener('afterSpecialReload', emitModified);
         });
     });
-};
 
-exports.enabled = false;
-exports.html = null;
-exports.port = null;
+    hotReload.childProcessEnv.HOT_RELOAD_BROWSER_REFRESH_PORT = port;
+
+    hotReload.on('reload', function() {
+        if (!hotReload.childProcess) {
+            return;
+        }
+
+        var deferred = q.defer();
+        hotReload.childProcess.once('message', function(message) {
+            hotReload.log('Server is ready. Triggering page refresh...');
+            if (message === readyEvent) {
+                deferred.resolve();
+            }
+        });
+        hotReload.waitFor(deferred.promise);
+    });
+};
--- refresh-tag.js
+++ refresh-tag.js
@@ -1,7 +1,15 @@
-var plugin = require('./index');
+var port = process.env.HOT_RELOAD_BROWSER_REFRESH_PORT;
+var enabled = !!port;
+var html;
+
+if (enabled) {
+    html = require('fs')
+        .readFileSync(require('path').join(__dirname, 'refresh.html'), {encoding: 'utf8'})
+        .replace(/PORT/g, port);
+}
 
 exports.render = function(input, context) {
-    if (plugin.enabled && input.enabled !== false) {
-        context.write(plugin.html);
+    if (enabled && input.enabled !== false) {
+        context.write(html);
     }
 };
